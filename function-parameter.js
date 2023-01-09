533d1d73158449b5163a6618eb68a00191a3fb7c
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-09 14:42:43 +0500

    Optional Parameter

--- function-parameter.js
+++
@@ -1,4 +0,0 @@
-function singTwo(first, second) {
-    console.log("".concat(first, " / ").concat(second));
-}
-singTwo("I Will Survive", "Higher Love");
---
+++ optional_parameter.ts
@@ -0,0 +1,8 @@
+function announceSong(song: string, singer?: string) {
+    console.log(`Song: ${song}`);
+    if (singer) {
+    console.log(`Singer: ${singer}`);
+    }
+    }announceSong("Greensleeves"); // Ok
+    announceSong("Greensleeves", undefined); // Ok
+    announceSong("Chandelier", "Sia"); // Ok
