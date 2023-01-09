e6ac8fae5fca3b5ff6003ee2dea0c3878cbcb98c
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-09 14:55:51 +0500

    Defult Parameter

--- optional_parameter.js
+++
@@ -1,9 +0,0 @@
-function announceSong(song, singer) {
-    console.log("Song: ".concat(song));
-    if (singer) {
-        console.log("Singer: ".concat(singer));
-    }
-}
-announceSong("Greensleeves"); // Ok
-announceSong("Greensleeves", undefined); // Ok
-announceSong("Chandelier", "Sia"); // Ok
