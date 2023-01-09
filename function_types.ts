7f1e058caa15b7b34aa9c1da5293fa85db776165
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-09 15:25:12 +0500

    Function Types

---
+++ function_types.ts
@@ -0,0 +1,10 @@
+const songs = ["Juice", "Shake It Off", "What's Up"];
+function runOnSongs(getSongAt: (index: number) => string) {
+for (let i = 0; i < songs.length; i += 1) {
+console.log(getSongAt(i));
+}
+}
+function getSongAt(index: number) {
+return `${songs[index]}`;
+}
+runOnSongs(getSongAt); // Ok
