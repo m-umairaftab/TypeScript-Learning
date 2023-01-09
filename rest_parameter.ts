c8fb54752e5b7f42ff13b9a48efbfc2ee31e150d
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-09 15:05:00 +0500

    Rest Parameter

---
+++ rest_parameter.ts
@@ -0,0 +1,7 @@
+function singAllTheSongs(singer: string, ...songs: string[]) {
+    for (const song of songs) {
+        console.log(`${song}, by ${singer}`);
+    }
+}
+singAllTheSongs("Alicia Keys"); // Ok
+singAllTheSongs("Lady Gaga", "Bad Romance", "Just Dance", "Poker Face"); // Ok
