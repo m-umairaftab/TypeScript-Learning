c3e5c8526fdd09d9ea1687c248be96a4f7e6e2da
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-20 11:46:20 +0500

    Test File

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-export const  num1 : number = 4
-export const num2 : number = 5
-let mul = num1 * num2
-console.log(mul);
+// function isNumberOrString(value: unknown): value is number {    
+//     return ['number'].includes(typeof value);
+// }
+// console.log(isNumberOrString('hello'));
