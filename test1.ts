4b648dafbae9cd823b74704598674e380c8c31ce
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-01-20 15:52:04 +0500

    Test File

--- test1.ts
+++ test1.ts
@@ -1,7 +1,7 @@
-import { num1,num2 } from "./test"
-console.log('num1: ',num1);
-console.log('num2: ',num2);
+// import { num1,num2 } from "./test"
+// console.log('num1: ',num1);
+// console.log('num2: ',num2);
 
-const num3 : number = 6
-let sum = num1 + num2 + num3
-console.log('sum: ',sum)
+// const num3 : number = 6
+// let sum = num1 + num2 + num3
+// console.log('sum: ',sum)
