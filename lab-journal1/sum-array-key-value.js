9c04e441777774c4fe64d1a82a91aaf9d68411e7
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-02-15 14:28:54 +0500

    Sum of array with key value

--- lab-journal1/sum-array-key-value.js
+++ lab-journal1/sum-array-key-value.js
@@ -1,13 +1,12 @@
+"use strict";
+exports.__esModule = true;
 var array = [1, 4, 10, 12, 15, 20, 22];
 var keyVal = 5;
-var arrayLength = array.length;
-console.log(arrayLength);
-array.forEach(sumOfKeyVal);
+var sumOfArray = 0;
+var total = array.forEach(sumOfKeyVal);
 function sumOfKeyVal(item) {
     if (item % keyVal === 0) {
-        var sumOfArray = 0;
-        sumOfArray += item;
-        console.log();
-        console.log(sumOfArray += sumOfArray);
+        console.log(item);
+        return item;
     }
 }
--- lab-journal1/sum-array-key-value.ts
+++ lab-journal1/sum-array-key-value.ts
@@ -1,16 +1,13 @@
+import { it } from "node:test"
+
 const array : number[] = [1,4,10,12,15,20,22]
 const keyVal : number = 5
-const arrayLength : number = array.length
-console.log(arrayLength)
-array.forEach(sumOfKeyVal)
-function sumOfKeyVal  (item){
+let sumOfArray : number = 0
+let total = array.forEach(sumOfKeyVal)
+function sumOfKeyVal  (item:number){
+    
     if (item % keyVal === 0) {
-        let sumOfArray : number = 0
-        sumOfArray += item
-        console.log();
-        console.log(sumOfArray+=sumOfArray);
-        
-        
+        console.log(item)
+        return item
     }
 }
-
